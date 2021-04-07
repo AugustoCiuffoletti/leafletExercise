f450ea26148cd3043bbf099a3ed91b6144e695ed
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-07 16:50:16 +0200

    1 file modified

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ import "./style.css";
 
 var n = 1;
 var elencoCoordinate = document.getElementById("coord");
+var coordinate = [];
 var mappa = L.map("mapid", {
   center: L.latLng(43.72301, 10.39663),
   zoom: 15,
   layers: [L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")]
 });
+function stampaCoordinate() {
+  let s = "";
+  for (i in coordinate ) {
+    s += (i+1) + ": " + coordinate[i].lat.toFixed(2) + ", " + coordinate[i].lat.toFixed(2) + "<br>";
+  }
+  return s;
+}
 mappa.on("click", e => {
   L.marker(e.latlng, { title: n }).addTo(mappa);
-  elencoCoordinate.innerHTML +=
-    n +
-    ": " +
-    e.latlng.lat.toFixed(5) +
-    ", " +
-    e.latlng.lng.toFixed(5) +
-    "<br>";
+  coordinate.push(e.latlng);
+  elencoCoordinate.innerHTML=stampaCoordinate();
   n++;
 });
