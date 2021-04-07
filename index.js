a7a0f0917d8be2920a78d1b8ae65d81df3055cac
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-07 16:16:50 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import './style.css';
 
+var listaCoordinate = document.getElementById("coord");
 var mappa = L.map('mapid',{
-  center: L.latLng(43.7229, 10.3966), 
+  center: L.latLng(43.72301, 10.39663), 
   zoom: 15,
   layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
       ]}
 );
 mappa.on('click', e =>
-  {alert("Hai puntato " + e.latlng.lat + "," + e.latlng.lng)});
+  {  
+    L.marker(e.latlng).addTo(mappa);
+    listaCoordinate.innerHTML += e.latlng.lat.toFixed(5) + ", " + e.latlng.lng.toFixed(5) + "<br>"
+   } )
