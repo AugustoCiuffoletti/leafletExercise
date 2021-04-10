164c419d1b36fd19bca49530581b5dc31644dc5a
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-10 17:59:36 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import './style.css';
 
-var mappa = L.map('mapid',{
+var aMap = L.map('mapid',{
   center: L.latLng(43.7229, 10.3966), 
   zoom: 15,
   layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
       ]}
 );
-var pallino = L.marker(L.latLng(43.72, 10.40));
-pallino.addTo(mappa);
-pallino.bindPopup("<b>Arrotondiamo!</b><br>Questo punto ha le coordinate arrotondate.");
-pallino.openPopup();
+var aMarker = L.marker(L.latLng(43.72, 10.40));
+aMarker.addTo(aMap);
+aMarker.bindPopup("<b>Rounding!</b><br>This point has coordinates rounded to 2 digits.<br>Notice the distance from the center map!.");
+aMarker.openPopup();
