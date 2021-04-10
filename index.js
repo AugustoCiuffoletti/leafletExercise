f28910052917ac2dab25f37f259a1e2f24809243
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-10 19:54:03 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,44 +1,32 @@
 import "./style.css";
 
 var n = 1;
-var elencoCoordinate = document.getElementById("coord");
-var bottoneSalva = document.getElementById("bottoneRegistra");
-var bottoneCarica = document.getElementById("bottoneCarica");
-
-var featuresCollection = {};
-featuresCollection.type = "FeatureCollection";
-featuresCollection.features = [];
-var mappa = L.map("mapid", {
+var markers = [];
+var aMap = L.map("mapid", {
   center: L.latLng(43.72301, 10.39663),
   zoom: 15,
   layers: [L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")]
 });
-function visualizzaCoordinate() {
-  elencoCoordinate.innerHTML = "";
-  for (let i in featuresCollection.features) {
-    let f = featuresCollection.features[i];
-    elencoCoordinate.innerHTML +=
-      Number(i) +
-      1 +
+
+function displayAllCoords() {
+  let displayCoord = document.getElementById("displayCoord");
+  displayCoord.innerHTML = "";
+  for (let i in markers) {
+    displayCoord.innerHTML +=
+      (Number(i)+1) +
       ": " +
-      f.geometry.coordinates[1].toFixed(5) +
+      markers[i].getLatLng().lat.toFixed(5) +
       ", " +
-      f.geometry.coordinates[0].toFixed(5) +
+      markers[i].getLatLng().lng.toFixed(5) +
       "<br>";
   }
 }
 
-mappa.on("click", e => {
-  let x = L.marker(e.latlng, { title: n }).addTo(mappa);
-  let feature = {};
-  feature.type = "Feature";
-  feature.title = n;
-  feature.geometry = {
-    type: "Point",
-    coordinates: [e.latlng.lng, e.latlng.lat]
-  };
-  featuresCollection.features.push(feature);
-  visualizzaCoordinate();
+
+aMap.on("click", e => {
+  let aMarker = L.marker(e.latlng, { title: n }).addTo(aMap);
+  markers.push(aMarker);
+  displayAllCoords();
   n++;
 });
 
@@ -52,25 +40,46 @@ newButton.onclick = e => {
     document.getElementById("newButton").style.display = "none";} );
 }
 
-bottoneSalva.onclick = e => {
+saveButton.onclick = e => {
+  let featuresCollection = {};
+  featuresCollection.type = "FeatureCollection";
+  featuresCollection.features = [];
+  for ( let i in markers) {
+    let feature = {};
+    feature.type = "Feature";
+    feature.title = i;
+    feature.geometry = {
+      type: "Point",
+      coordinates: [markers[i].getLatLng().lng, markers[i].getLatLng().lat]
+    };
+    featuresCollection.features.push(feature);
+  }
   let url = document.getElementById("urlBox").value;
-  n = featuresCollection.features.length + 1;
   fetch(url, {
     method: "POST",
     body: JSON.stringify(featuresCollection)
   });
 };
 
-bottoneCarica.onclick = e => {
+loadButton.onclick = e => {
+  for ( let i in markers ) {
+    aMap.removeLayer(markers[i]);
+  }
+  markers=[];
   let url = document.getElementById("urlBox").value;
+  // QUI NON FUNZIONA
   fetch(url)
   .then( response => response.json())
-  .then( dati => {
-    featuresCollection = dati;
-    n = featuresCollection.features.length + 1;
-    L.geoJSON(featuresCollection, {
-      onEachFeature: (f, l) => (l.options.title = f.title)
-    }).addTo(mappa);
+  .then( payload => {
+    console.log(payload);
+    features = payload.features;
+    console.log(features);
+    n = features.length + 1;
+    for ( let i in features ) {
+      let coord = L.latLng(features[i].lat,features[i].lng);
+      let aMarker = L.marker( coord, { title: i }).addTo(aMap);
+      markers.push(aMarker)
+    }
     visualizzaCoordinate();
   });
 };
