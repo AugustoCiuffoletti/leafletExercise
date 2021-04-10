3ea957cdc38f37b2269e6c330db8d6097e2d6c9a
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-10 18:09:21 +0200

    2 files modified

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var aMap = L.map("mapid", {
   zoom: 15,
   layers: [L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")]
 });
-mappa.on("click", e => {
+aMap.on("click", e => {
   let displayCoord = document.getElementById("displayCoord");
   let aMarker = L.marker(e.latlng).addTo(aMap);
   displayCoord.innerHTML +=
