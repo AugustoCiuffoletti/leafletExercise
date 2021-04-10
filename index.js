5eee4226ee9824d750adc9c3dc029071e8c13cfb
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-10 18:23:01 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 import "./style.css";
 
 var n = 1;
-var elencoCoordinate = document.getElementById("coord");
-var coordinate = [];
-var mappa = L.map("mapid", {
+var markers = [];
+var aMap = L.map("mapid", {
   center: L.latLng(43.72301, 10.39663),
   zoom: 15,
   layers: [L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")]
 });
-function visualizzaCoordinate() {
-  elencoCoordinate.innerHTML = "";
-  for (let i in coordinate) {
-    elencoCoordinate.innerHTML +=
+
+function displayAllCoords() {
+  let displayCoord = document.getElementById("displayCoord");
+  displayCoord.innerHTML = "";
+  for (let i in markers) {
+    displayCoord.innerHTML +=
       (Number(i)+1) +
       ": " +
-      coordinate[i].lat.toFixed(5) +
+      markers[i].getLngLat().toFixed(5) +
       ", " +
-      coordinate[i].lng.toFixed(5) +
+      markers[i].getLngLat().toFixed(5) +
       "<br>";
   }
 }
-mappa.on("click", e => {
-  L.marker(e.latlng, { title: n }).addTo(mappa);
-  coordinate.push(e.latlng);
-  visualizzaCoordinate();
+
+aMap.on("click", e => {
+  let aMarker = L.marker(e.latlng, { title: n }).aMarker.addTo(aMap);
+  markers.push(aMarker);
+  displayAllCoords();
   n++;
 });
