04db579b830115b7fffa926cda9d583cdc141b62
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-11 11:12:36 +0200

    1 file modified

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ var aMap = L.map("mapid", {
   zoom: 15,
   layers: [L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")]
 });
+
 aMap.on("click", e => {
   let displayCoord = document.getElementById("displayCoord");
   let aMarker = L.marker(e.latlng).addTo(aMap);
   displayCoord.innerHTML +=
-    aMarker.getLatLng().lat.toFixed(5) + ", " + 
-    aMarker.getLatLng().lng.toFixed(5) + "<br>";
+    aMarker.getLatLng().lat.toFixed(5) +
+    ", " +
+    aMarker.getLatLng().lng.toFixed(5) +
+    "<br>";
 });
