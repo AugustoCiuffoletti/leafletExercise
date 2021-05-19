a66fc381b0c3eebde3fedeeb2037c470356aaf49
AugustoCiuffoletti <augusto@di.unipi.it>
2021-05-19 16:26:20 +0200

    1 file modified

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var centerLng = 10.39663;
 // Coordinates for the point feature
 var pointLat = centerLat.toFixed(2);
 var pointLng = centerLng.toFixed(2);
-
 // Define a map
 var aMap = L.map('mapid', {
   center: L.latLng(centerLat, centerLng),
@@ -15,6 +14,7 @@ var aMap = L.map('mapid', {
 });
 // Define (using a constructor) a point feature
 var aMarker = L.marker(L.latLng(pointLat, pointLng));
+
 // Attach a popup
 aMarker.bindPopup(
   '<b>Rounding!</b><br>This point has coordinates rounded to 2 digits.<br>Notice the distance from the Tower!'
