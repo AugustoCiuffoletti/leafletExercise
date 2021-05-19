b4e69b1ff96a7e8167a57099cbe0b07fcdb2c492
AugustoCiuffoletti <augusto@di.unipi.it>
2021-05-19 16:28:57 +0200

    1 file modified

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ var aMap = L.map('mapid', {
   zoom: 15,
   layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')]
 });
+
 // Attach a mre complex callback to the click event
 aMap.on('click', e => {
   // Get a handle for the DOM element containing the list of coordinates
   let displayCoord = document.getElementById('displayCoord');
   // Add a marker with the coordinates in the event descriptor
   let aMarker = L.marker(e.latlng).addTo(aMap);
-  // Concatenate the neww coordinates in the DOM element
+  // Concatenate the new coordinates in the DOM element
   displayCoord.innerHTML +=
     aMarker.getLatLng().lat.toFixed(5) + // truncate coordinates
     ', ' +
