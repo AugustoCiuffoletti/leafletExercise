40eb5aba4807e31add5be0c4fc56ee6b7910e7cc
AugustoCiuffoletti <augusto@di.unipi.it>
2021-05-19 15:46:28 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import './style.css';
 
-var aMap = L.map('mapid',{
-  center: L.latLng(43.72301, 10.39663), 
+// Coordinates for the tower of Pisa
+var centerLat = 43.72301;
+var centerLong = 10.39663;
+// Display the map
+var aMap = L.map('mapid', {
+  center: L.latLng(centerLat, centerLong),
   zoom: 15,
-  layers: [
-        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
-      ]}
-);
-
+  layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')]
+});
+// Attach a callback to the click event. The callback displays the alert
 aMap.on('click', e =>
-  alert("Hai puntato " + e.latlng.lat + "," + e.latlng.lng)
-  );
+  alert('Hai puntato ' + e.latlng.lat + ',' + e.latlng.lng)
+);
