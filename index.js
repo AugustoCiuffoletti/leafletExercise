12b35a740c0932c6c63ff23d8f1370075514598a
AugustoCiuffoletti <augusto@di.unipi.it>
2021-05-19 16:19:42 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-import "./style.css";
+import './style.css';
 
-var aMap = L.map("mapid", {
-  center: L.latLng(43.72301, 10.39663),
+// Coordinates for the tower of Pisa
+var centerLat = 43.72301;
+var centerLong = 10.39663;
+// Display the map
+var aMap = L.map('mapid', {
+  center: L.latLng(centerLat, centerLong),
   zoom: 15,
-  layers: [L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")]
+  layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')]
 });
-
-aMap.on("click", e => {
-  let displayCoord = document.getElementById("displayCoord");
+// Attach a mre complex callback to the click event
+aMap.on('click', e => {
+  // Get a handle for the DOM element containing the list of coordinates
+  let displayCoord = document.getElementById('displayCoord');
+  // Add a marker with the coordinates in the event descriptor
   let aMarker = L.marker(e.latlng).addTo(aMap);
+  // Concatenate the neww coordinates in the DOM element
   displayCoord.innerHTML +=
-    aMarker.getLatLng().lat.toFixed(5) +
-    ", " +
+    aMarker.getLatLng().lat.toFixed(5) + // truncate coordinates
+    ', ' +
     aMarker.getLatLng().lng.toFixed(5) +
-    "<br>";
+    '<br>';
 });
