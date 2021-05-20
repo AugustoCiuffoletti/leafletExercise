0148c254f802c5fead3e15d1e3ea9a227fa4c786
AugustoCiuffoletti <augusto@di.unipi.it>
2021-05-20 14:13:15 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 import "./style.css";
 
-var markers = [];
-var aMap = L.map("mapid", {
-  center: L.latLng(43.72301, 10.39663),
+// Coordinates for the tower of Pisa
+var centerLat = 43.72301;
+var centerLong = 10.39663;
+// Display the map
+var aMap = L.map('mapid', {
+  center: L.latLng(centerLat, centerLong),
   zoom: 15,
   layers: [L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")]
 });
-
-function displayAllCoords() {
-  let displayCoord = document.getElementById("displayCoord");
-  displayCoord.innerHTML = "";
-  markers.forEach( (marker,i) => {
-    displayCoord.innerHTML +=
-      (Number(i)+1) +
-      ": " +
-      marker.getLatLng().lat.toFixed(5) +
-      ", " +
-      marker.getLatLng().lng.toFixed(5) +
-      "<br>";
-  } )
-}
+// An array of markers
+var markers = L.layerGroup();
+// Add controls for the layer
+L.control.layers(
+  {},                  // base layers, radio buttons
+  {"Markers": markers} // overlay layers, checkbox buttons
+).addTo(aMap);
 
 aMap.on("click", e => {
-  let aMarker = L.marker(e.latlng).addTo(aMap);
-  markers.push(aMarker);
-  displayAllCoords();
+  let n = markers.getLayers().length + 1;
+  let displayCoord = document.getElementById('displayCoord');
+  let aMarker = L.marker(e.latlng, { title: n }).addTo(aMap);
+  markers.addLayer(aMarker);
+  displayCoord.innerHTML +=
+    n +
+    ': ' +
+    aMarker.getLatLng().lat.toFixed(5) +
+    ', ' +
+    aMarker.getLatLng().lng.toFixed(5) +
+    '<br>';
 });
 
 newButton.onclick = e => {
@@ -34,5 +38,7 @@ newButton.onclick = e => {
   .then( response => response.text() )
   .then( body => {
     document.getElementById("urlBox").value = body;
-    document.getElementById("newButton").style.display = "none";} );
+    document.getElementById("newButton").style.display = "none";
+    
+    } );
 }
