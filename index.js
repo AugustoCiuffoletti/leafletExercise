f2146890fa9b014b4fdc159cc863e5795cdb7ba8
AugustoCiuffoletti <augusto@di.unipi.it>
2021-05-20 09:53:07 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import "./style.css";
+import './style.css';
 
 // Coordinates for the tower of Pisa
 var centerLat = 43.72301;
@@ -7,18 +7,20 @@ var centerLong = 10.39663;
 var aMap = L.map('mapid', {
   center: L.latLng(centerLat, centerLong),
   zoom: 15,
-  layers: [L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")]
+  layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')]
 });
+// Markers count
+var n = 1;
 
-aMap.on("click", e => {
-  let displayCoord = document.getElementById("displayCoord");
+aMap.on('click', e => {
+  let displayCoord = document.getElementById('displayCoord');
   let aMarker = L.marker(e.latlng, { title: n }).addTo(aMap);
   displayCoord.innerHTML +=
     n +
-    ": " +
+    ': ' +
     aMarker.getLatLng().lat.toFixed(5) +
-    ", " +
+    ', ' +
     aMarker.getLatLng().lng.toFixed(5) +
-    "<br>";
+    '<br>';
   n++;
 });
