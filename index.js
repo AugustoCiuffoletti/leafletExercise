f913ee0257238da4dce2a9e4ba32f9cbb43eda15
AugustoCiuffoletti <augusto@di.unipi.it>
2021-05-20 14:52:17 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,65 +1,99 @@
-import "./style.css";
+import './style.css';
 
-var markers = [];
-var aMap = L.map("mapid", {
-  center: L.latLng(43.72301, 10.39663),
+// Coordinates for the tower of Pisa
+var centerLat = 43.72301;
+var centerLong = 10.39663;
+// Display the map
+var aMap = L.map('mapid', {
+  center: L.latLng(centerLat, centerLong),
   zoom: 15,
-  layers: [L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")]
+  layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')]
 });
+// An array of markers
+var markers = L.layerGroup();
+markers.addTo(aMap);
+// Add controls for the layer
+L.control
+  .layers(
+    {}, // base layers, radio buttons
+    { Markers: markers } // overlay layers, checkbox buttons
+  )
+  .addTo(aMap);
 
-function displayAllCoords() {
-  let displayCoord = document.getElementById("displayCoord");
-  displayCoord.innerHTML = "";
-  markers.forEach( (marker,i) => {
-    displayCoord.innerHTML +=
-      (Number(i)+1) +
-      ": " +
-      marker.getLatLng().lat.toFixed(5) +
-      ", " +
-      marker.getLatLng().lng.toFixed(5) +
-      "<br>";
-  } )
-}
-
-aMap.on("click", e => {
-  let aMarker = L.marker(e.latlng).addTo(aMap);
-  markers.push(aMarker);
-  displayAllCoords();
+aMap.on('click', e => {
+  let n = markers.getLayers().length + 1;
+  let displayCoord = document.getElementById('displayCoord');
+  let aMarker = L.marker(e.latlng, { title: n }).addTo(aMap);
+  markers.addLayer(aMarker);
+  displayCoord.innerHTML +=
+    n +
+    ': ' +
+    aMarker.getLatLng().lat.toFixed(5) +
+    ', ' +
+    aMarker.getLatLng().lng.toFixed(5) +
+    '<br>';
 });
 
 newButton.onclick = e => {
-  fetch("https://api.keyvalue.xyz/new/myKey", {
-    method: "POST"
+  fetch('https://api.keyvalue.xyz/new/dhss2021', {
+    method: 'POST'
   })
-  .then( response => response.text() )
-  .then( body => {
-    document.getElementById("urlBox").value = body;
-    document.getElementById("newButton").style.display = "none";} );
+    .then(response => response.text())
+    .then(body => {
+      let url = body;
+      document.getElementById('urlBox').value = url;
+      console.log();
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(JSON.stringify(markers.toGeoJSON()))
+      }).then(
+        body => {
+          document.getElementById('newButton').style.display = 'none';
+          document.getElementById('saveButton').style.display = 'true';
+          document.getElementById('loadButton').style.display = 'true';
+        },
+        err => console.log(err)
+      );
+    });
+};
+
+function displayAllCoords() {
+  let displayCoord = document.getElementById('displayCoord');
+  displayCoord.innerHTML = '';
+  markers.forEach((marker, i) => {
+    displayCoord.innerHTML +=
+      Number(i) +
+      1 +
+      ': ' +
+      marker.getLatLng().lat.toFixed(5) +
+      ', ' +
+      marker.getLatLng().lng.toFixed(5) +
+      '<br>';
+  });
 }
 
 saveButton.onclick = e => {
-  let url = document.getElementById("urlBox").value;
-  let coordinates = markers.map( m => m.getLatLng() );
+  let url = document.getElementById('urlBox').value;
   fetch(url, {
-    method: "POST",
-    body: JSON.stringify(coordinates)
+    method: 'POST',
+    body: JSON.stringify(JSON.stringify(markers.toGeoJSON()))
   });
 };
 
 loadButton.onclick = e => {
-  for ( let i in markers ) {
+  for (let i in markers) {
     aMap.removeLayer(markers[i]);
   }
-  markers=[];
-  let url = document.getElementById("urlBox").value;
+  markers = [];
+  let url = document.getElementById('urlBox').value;
   fetch(url)
-  .then( response => response.json() )
-  .then( payload => {
-    let coordinates = payload;
-    for ( let i in coordinates ) {
-      let aMarker = L.marker( coordinates[i]).addTo(aMap);
-      markers.push(aMarker)
-    }
-    displayAllCoords();
-  } )
+    .then(response => response.json())
+    .then(payload => {
+      let coordinates = payload;
+      for (let i in coordinates) {
+        let aMarker = L.marker(coordinates[i]).addTo(aMap);
+        markers.push(aMarker);
+      }
+      displayAllCoords();
+    });
 };
