494a7e943b17ffd900ffe8aa81e2a6efe845328b
AugustoCiuffoletti <augusto@di.unipi.it>
2021-05-20 14:32:36 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import "./style.css";
+import './style.css';
 
 // Coordinates for the tower of Pisa
 var centerLat = 43.72301;
@@ -7,17 +7,20 @@ var centerLong = 10.39663;
 var aMap = L.map('mapid', {
   center: L.latLng(centerLat, centerLong),
   zoom: 15,
-  layers: [L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")]
+  layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')]
 });
 // An array of markers
 var markers = L.layerGroup();
+markers.addTo(aMap);
 // Add controls for the layer
-L.control.layers(
-  {},                  // base layers, radio buttons
-  {"Markers": markers} // overlay layers, checkbox buttons
-).addTo(aMap);
+L.control
+  .layers(
+    {}, // base layers, radio buttons
+    { Markers: markers } // overlay layers, checkbox buttons
+  )
+  .addTo(aMap);
 
-aMap.on("click", e => {
+aMap.on('click', e => {
   let n = markers.getLayers().length + 1;
   let displayCoord = document.getElementById('displayCoord');
   let aMarker = L.marker(e.latlng, { title: n }).addTo(aMap);
@@ -32,16 +35,20 @@ aMap.on("click", e => {
 });
 
 newButton.onclick = e => {
-  fetch("https://api.keyvalue.xyz/new/myKey", {
-    method: "POST"
+  fetch('https://api.keyvalue.xyz/new/dhss2021', {
+    method: 'POST'
   })
-  .then( response => response.text() )
-  .then( body => {
-    document.getElementById("urlBox").value = body;
-    document.getElementById("newButton").style.display = "none";
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify(markers.toGeoJSON())
+    .then(response => response.text())
+    .then(body => {
+      let url = body;
+      document.getElementById('urlBox').value = url;
+      console.log();
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(JSON.stringify(markers.toGeoJSON()))
+      }).then(
+        body => (document.getElementById('newButton').style.display = 'none'),
+        err => console.log(err)
+      );
     });
-  });
-}
+};
