17d417bd9a3af5b4e396d2a19edf03ccb0a9bea6
AugustoCiuffoletti <augusto@di.unipi.it>
2021-05-28 14:55:44 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import './style.css';
 // Coordinates for the tower of Pisa
 var centerLat = 43.72301;
 var centerLong = 10.39663;
-var baseURL = "https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/dhss21-mczua/service/svc/incoming_webhook"
+var baseURL =
+  'https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/dhss21-mczua/service/svc/incoming_webhook';
 
 // Display the map
 var aMap = L.map('mapid', {
@@ -37,23 +38,23 @@ aMap.on('click', e => {
 });
 
 newButton.onclick = e => {
-  fetch(baseURL+'/getKey', { method: 'POST' })
-  .then(response => response.text())
-  .then(body => {
-    let key = JSON.parse(body);
-    fetch(baseURL+'/setValue'+'?key='+key, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(markers.toGeoJSON())
-    }).then(
-      () => {
-        console.log("Success");
-        document.getElementById('keyBox').value = key;
-        document.getElementById('newButton').style.display = 'none'
-      },
-      err => console.log(err)
-    );
-  });
+  fetch(baseURL + '/getKey', { method: 'POST' })
+    .then(response => response.text())
+    .then(body => {
+      let key = JSON.parse(body);
+      fetch(baseURL + '/setValue' + '?key=' + key, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(markers.toGeoJSON())
+      }).then(
+        () => {
+          console.log('Success');
+          document.getElementById('keyBox').value = key;
+          document.getElementById('newButton').style.display = 'none';
+        },
+        err => console.log(err)
+      );
+    });
 };
 
 saveButton.onclick = e => {
@@ -70,13 +71,14 @@ saveButton.onclick = e => {
 loadButton.onclick = e => {
   let key = document.getElementById('keyBox').value;
   let displayCoord = document.getElementById('displayCoord');
-  displayCoord.innerHTML = "";
+  displayCoord.innerHTML = '';
   markers.clearLayers();
   fetch(baseURL + '/getValue' + '?key=' + key)
     .then(response => response.json())
     .then(payload => {
       let layer = JSON.parse(payload);
       for (let i in layer.features) {
+        console.log('+');
         let coord = L.latLng([
           layer.features[i].geometry.coordinates[1],
           layer.features[i].geometry.coordinates[0]
@@ -84,7 +86,8 @@ loadButton.onclick = e => {
         let aMarker = L.marker(coord, { title: Number(i) + 1 });
         markers.addLayer(aMarker);
         displayCoord.innerHTML +=
-          ( Number(i) + 1 ) +
+          Number(i) +
+          1 +
           ': ' +
           coord.lat.toFixed(5) +
           ', ' +
